0b9d8df4d82783b3747808baa61c1cdc35e532a2
VinhTocDo <86400141+VinhTocDo@users.noreply.github.com>
2021-06-24 14:20:54 +0700

    Add files via upload

---
+++ SolveEquation.java
@@ -0,0 +1,21 @@
+package Tong;
+
+public class SolveEquation {
+	public static String linearEquation(int number1, int
+			number2) {
+
+		if (number1 == 0)
+		if (number2 == 0)
+			return "Multi roots";
+		else
+			return "No root";
+		else
+			return "One root";
+
+	}
+
+	public void setNumber(int i) {
+		// TODO Auto-generated method stub
+		
+	}
+}
